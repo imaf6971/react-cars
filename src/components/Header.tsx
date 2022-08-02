59503aac10081984069933b48039677b0d98616c
Vladislav Mironov <imaf6971@gmail.com>
2022-08-02 03:43:41 +0300

    wrapped header with container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Header.sass'
 
 export default function Header() {
   return (
-	<header className='header'>
-		<img src="images/logo.svg" alt="Logo" className="logo" />
-		<nav className="menu"> </nav>
-		<img src="images/menu.svg" alt="Menu" className="menu" />
-		
-	</header>
+	<div className="container">
+		<header className='header'>
+			<img src="images/logo.svg" alt="Logo" className="logo" />
+			<nav className="menu"> </nav>
+			<img src="images/menu.svg" alt="Menu" className="menu" />
+		</header>
+	</div>
   )
 }
