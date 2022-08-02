8d8a0191ffe664b2830c462ea5e7509d2001f6e0
Vladislav Mironov <imaf6971@gmail.com>
2022-08-02 03:35:26 +0300

    added basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+	<header className='header'>
+		<img src="images/logo.svg" alt="Logo" className="logo" />
+		<nav className="menu"> </nav>
+		<img src="images/menu.svg" alt="Menu" className="menu" />
+		
+	</header>
+  )
+}
